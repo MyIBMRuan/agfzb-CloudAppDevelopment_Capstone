d31e84574734eb35baf9ed4c422b15d7b4329644
Ruan191 <111080450+MyIBMRuan@users.noreply.github.com>
2022-08-18 20:45:46 +0200

    authentication

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["ruanmyibm.eu-gb.cf.appdomain.cloud"]
+ALLOWED_HOSTS = ["127.0.0.1"]#["ruanmyibm.eu-gb.cf.appdomain.cloud"]
 
 
 # Application definition
